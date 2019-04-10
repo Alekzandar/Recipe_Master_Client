abd2dc079909d9452aa3fe7ca94cd367e7b6828e
Aleksandar Antonov <alekzandar@live.com>
2019-04-09 21:09:42 -0400

    Button to load in recipes

--- recipe-master-client/src/app/aleksandar-view/aleksandar-view.component.ts
+++ recipe-master-client/src/app/aleksandar-view/aleksandar-view.component.ts
@@ -14,22 +14,23 @@ import { of } from 'rxjs';
 export class AleksandarViewComponent implements OnInit {
   recipesList: Observable<Recipe[]>;
   private respRecipes;
+  
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.loadRecipes().subscribe(
-      (data) => {
-        this.recipesList = of(data); // WORKS AND ALWAYS UP TO DATE
-        console.log(this.recipesList); // ONLY WORKS ONCE
-      }
-    );
    // let headers = new HttpHeaders();
    // headers = headers.set('X-RapidAPI-Host', 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com').set('X-RapidAPI-Key','653c9ff2demsh033c83b0a49033ep1855d1jsnaebe6c7b70e9');
   
   }
 
-  loadRecipes():  Observable<Recipe[]> {
-    return this.recipeService.getRecipes();
-  }
+  loadRecipes():  void {
+    this.recipeService.getRecipes().subscribe(
+      (data) => {
+        this.recipesList = of(data); 
+        console.log(this.recipesList);
+      }
+    );
   
+  
+}
 }
