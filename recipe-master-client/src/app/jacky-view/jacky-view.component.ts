e88aa402e6be670c682f7f693bade2ff30dbd45d
unknown <iapchi86@gmail.com>
2019-04-11 16:21:38 -0400

    in progress

--- recipe-master-client/src/app/jacky-view/jacky-view.component.ts
+++ recipe-master-client/src/app/jacky-view/jacky-view.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { RecipeService } from './../services/recipe.service';
+import { Observable } from 'rxjs';
+import { Recipe } from '../objects/recipe';
+import { of } from 'rxjs';
 
 @Component({
-  selector: 'app-jacky-view',
-  templateUrl: './jacky-view.component.html',
-  styleUrls: ['./jacky-view.component.css']
+	selector: 'app-jacky-view',
+	templateUrl: './jacky-view.component.html',
+	styleUrls: ['./jacky-view.component.css']
 })
+
 export class JackyViewComponent implements OnInit {
+	private recipesObs: Observable<Recipe[]>;
+	private recipesList: Recipe[];
 
-  constructor() { }
+	names = '';
+	constructor(private recipeService: RecipeService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	loadRecipes(): void {
+		console.log(this.names);
+		this.recipeService.getRecipes().subscribe(
+			resp => {
+				this.recipesList = resp as Recipe[];
+				console.log(this.recipesList);
+			}
+		);
+
+	}
 }
--- recipe-master-client/src/app/services/recipe.service.ts
+++ recipe-master-client/src/app/services/recipe.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { JackyViewComponent } from './../jacky-view/jacky-view.component';
 
 
 
 const recipeHeaders = {
   headers: new HttpHeaders({
     "X-RapidAPI-Host" : "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": "653c9ff2demsh033c83b0a49033ep1855d1jsnaebe6c7b70e9"
+    "X-RapidAPI-Key": "7b8987c75emsh31f353e878d74e2p1e424fjsn5ffa201e13e5"+null
   })
 };
 
@@ -19,8 +20,9 @@ const recipeHeaders = {
 export class RecipeService {
 
   constructor(private http: HttpClient) { }
+  /*names: string;*/
 
-  recipeUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%2Cflour%2Csugar';
+  recipeUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients='+this.names;
 
 
   getRecipes() {
