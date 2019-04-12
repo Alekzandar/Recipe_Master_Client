48840e248b21634322807ba8862487f8ee1e6479
tnawshin <tawbin.nawshin@baruchmail.cuny.edu>
2019-04-12 15:40:15 -0400

    another push after editing

--- recipe-master-client/src/app/services/user-register.service.ts
+++ recipe-master-client/src/app/services/user-register.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { User } from './../objects/user';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-=======
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
->>>>>>> c4697b37d6abf0eb597dd31bebf71f9d4acddcd5
-
 
 const registerHeaders = {
   headers: new HttpHeaders({
@@ -35,8 +29,4 @@ const registerHeaders = {
     }
   }
 
-<<<<<<< HEAD
-=======
-  constructor(private http: HttpClient) {
->>>>>>> c4697b37d6abf0eb597dd31bebf71f9d4acddcd5
 
