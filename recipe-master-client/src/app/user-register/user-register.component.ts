f4619328560f7b3f88f23ba3d298c18cf803f035
Aleksandar Antonov <alekzandar@live.com>
2019-04-16 12:51:43 -0400

    Routing cleanup

--- recipe-master-client/src/app/user-register/user-register.component.ts
+++ recipe-master-client/src/app/user-register/user-register.component.ts
@@ -56,7 +56,7 @@ export class UserRegisterComponent implements OnInit {
     this.registerService.saveUser(this.u).subscribe();
     this.resetUser();
     u.ErrorText = "Form Submitted Successfully!!!";
-    window.location.href = '#';
+    window.location.href = '/main';
 
   }
 }
