5f9c97d46f6c43bcb8862bcab9f8a67e9bb0fced
unknown <iapchi86@gmail.com>
2019-04-17 19:09:41 -0400

    functionality all done

--- recipe-master-client/src/app/recipe-detail/recipe-detail.component.ts
+++ recipe-master-client/src/app/recipe-detail/recipe-detail.component.ts
@@ -7,7 +7,7 @@ import { DetailService } from './../services/detail.service';
 })
 export class RecipeDetailComponent implements OnInit {
   constructor(private detailService: DetailService) { }
-	private id: string = '479101';
+	private id: string = '289101';
 
 	private recipe;
 	private recipeTitle;
@@ -25,35 +25,46 @@ export class RecipeDetailComponent implements OnInit {
 	private IList = [];
 	private extendedIngredientsName = [];
 	private recipeDirection;
+	private ketogenic;
+	private ppServing;
 
 
 	getDetails(){
 		this.detailService.getObject(this.id).subscribe(
 			resp => {
-				console.log(resp+"GOT RESPONSE");
+				//console.log(resp+"GOT RESPONSE");
 /*				this.recipeKey = Object.keys(resp);
 				console.log(this.recipeKey);
 				this.recipe = Object.values(resp);
 				console.log(this.recipe);*/
 				this.recipeEntries = resp;
-				console.log(this.recipeEntries);
+				//console.log(this.recipeEntries);
 /*				Object.entries(this.recipeEntries.extendedIngredients).forEach(key=>
 				{ console.log(key.names); })*/
 				Object.entries(this.recipeEntries.extendedIngredients).forEach(
-					([key, value]) => /*console.log(Object.values(value)[4])*/
-						this.extendedIngredientsName.push(Object.values(value)[4])
+					([key, value]) => /*console.log(Object.values(value)[4]) ingredient names only*/
+						this.extendedIngredientsName.push(Object.values(value)[5])
 				);
-				console.log(this.extendedIngredientsName);
+				//console.log(this.extendedIngredientsName);
 				this.recipeTitle = this.recipeEntries.title;
-				console.log(this.recipeTitle);
-				this.recipeServings = this.recipeEntries.servings;
-				console.log(this.recipeServings);
+				//console.log(this.recipeTitle);
 				this.recipeCheap = this.recipeEntries.cheap;
-				console.log(this.recipeCheap);
+				//console.log(this.recipeCheap);
 				this.recipeImage = this.recipeEntries.image;
-				console.log(this.recipeImage);
+				//console.log(this.recipeImage);
+				this.recipeServings = this.recipeEntries.servings;
+				//console.log(this.recipeServings);
 				this.recipeDirection = this.recipeEntries.instructions;
-				console.log(this.recipeDirection);
+				//console.log(this.recipeDirection);
+				this.vegan = this.recipeEntries.vegan;
+				//console.log(this.vegan);
+				this.vegetarian = this.recipeEntries.vegatarian;
+				this.prepTime = this.recipeEntries.readyInMinutes;
+				this.dairyFree = this.recipeEntries.dairyFree;
+				//console.log(this.dairyFree);
+				this.glutenFree = this.recipeEntries.glutenFree;
+				this.ketogenic = this.recipeEntries.ketogenic;
+				this.ppServing = this.recipeEntries.pricePerServing;
 /*				this.recipeImage = this.recipeEntries.extendedIngredients[0].name;
 				console.log(this.recipeImage);
 				this.extendedIngredients = this.recipeEntries.extendedIngredients;
